d8204c78757ff5ddf56540b2b515210eb7dfecab
ag143 <ag14341@gmail.com>
2024-09-17 23:07:45 +0530

    Update k8.sh

--- k8.sh
+++ k8.sh
@@ -1,7 +1,7 @@
-source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
-echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
-alias k=kubectl
-complete -o default -F __start_kubectl k
+#source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
+#echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+#alias k=kubectl
+#complete -o default -F __start_kubectl k
 kubectl config view # Show Merged kubeconfig settings.
 
 # use multiple kubeconfig files at the same time and view merged config
