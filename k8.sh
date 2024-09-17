8356e2e99a5178ef5a679f8d60e4af3e7418c112
ag143 <ag14341@gmail.com>
2024-09-17 23:09:22 +0530

    Update k8.sh

--- k8.sh
+++ k8.sh
@@ -23,18 +23,18 @@ kubectl config view -o jsonpath='{.users[*].name}'   # get a list of users
 kubectl config get-contexts                          # display list of contexts
 kubectl config get-contexts -o name                  # get all context names
 kubectl config current-context                       # display the current-context
-kubectl config use-context my-cluster-name           # set the default context to my-cluster-name
+# kubectl config use-context my-cluster-name           # set the default context to my-cluster-name
 
-kubectl config set-cluster my-cluster-name           # set a cluster entry in the kubeconfig
+# kubectl config set-cluster my-cluster-name           # set a cluster entry in the kubeconfig
 
 # configure the URL to a proxy server to use for requests made by this client in the kubeconfig
-kubectl config set-cluster my-cluster-name --proxy-url=my-proxy-url
+# kubectl config set-cluster my-cluster-name --proxy-url=my-proxy-url
 
 # add a new user to your kubeconf that supports basic auth
-kubectl config set-credentials kubeuser/foo.kubernetes.com --username=kubeuser --password=kubepassword
+# kubectl config set-credentials kubeuser/foo.kubernetes.com --username=kubeuser --password=kubepassword
 
 # permanently save the namespace for all subsequent kubectl commands in that context.
-kubectl config set-context --current --namespace=ggckad-s2
+# kubectl config set-context --current --namespace=ggckad-s2
 
 # set a context utilizing a specific username and namespace.
 #kubectl config set-context gce --user=cluster-admin --namespace=foo \
